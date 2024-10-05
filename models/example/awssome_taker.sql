4b82a961851ba2328fb5c27c5bdf71b45056e897
前原武 <takeru.maehara@techtouch.co.jp>
2024-10-05 05:06:23 +0000

    tt

--- models/example/awssome_taker.sql
+++ models/example/awssome_taker.sql
@@ -11,7 +11,7 @@ select
     c_nationkey,
     c_phone,
     count(distinct o_orderkey) * 2 as order_count,
-    sum(o_totalprice) *  2as order_price
+    sum(o_totalprice) *  2 as order_price
 from customer as c
 join orders as o on c.c_custkey = o.o_custkey
 group by all
