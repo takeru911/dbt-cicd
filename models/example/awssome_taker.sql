53982f4dde7d41bb3d0193d7f6b02f21c187e2a2
前原武 <takeru.maehara@techtouch.co.jp>
2024-10-05 04:13:49 +0000

    test

---
+++ models/example/awssome_taker.sql
@@ -0,0 +1,17 @@
+with customer as (
+    select * from {{source('sample_data', 'customer')}}
+)
+,orders as (
+    select * from {{source('sample_data','orders')}}
+)
+
+select 
+    c_custkey,
+    c_name,
+    c_nationkey,    
+    count(distinct o_orderkey) as order_count,
+    sum(o_totalprice) as order_price
+from customer as c
+join orders as o on c.c_custkey = o.o_custkey
+group by all
+order by 1,2,3
